e1ba578d472b57ff0f1f1735d2617c4e2cf8fe1a
Ricardoamg05 <ricardo_amg8@hotmail.com>
2024-01-13 12:00:16 -0400

    first commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
-class homepageview(TemplateView):
+class homepageview(ListView):
+    model= Post
     template_name="home.html"
 
 
 class aboutpageview(TemplateView):
     template_name="about.html"
-# Create your views here.
 
