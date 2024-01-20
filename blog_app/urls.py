e933360dad5e35858977639d0708e7db71a3bbb4
Ricardoamg05 <ricardo_amg8@hotmail.com>
2024-01-20 11:39:14 -0400

    create read

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import homepageview, aboutpageview
-
+from .views import homepageview, aboutpageview, BlogDetailView, BlogCreateView
 urlpatterns=[
     path("",  homepageview.as_view(), name="home"),
-    path("about/", aboutpageview.as_view(), name="about" ) 
-
+    path("about/", aboutpageview.as_view(), name="about" ),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name= 'post_detail'),
+    path("post/new", BlogCreateView.as_view(), name= "post_new")
 ]
